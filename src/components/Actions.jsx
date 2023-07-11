afa99a53a53286e8a5f3c2be36544d3079bac81f
Roman Kuptsov <romankuptsov@icloud.com>
2023-07-11 17:06:34 +0200

    solved

---
+++ src/components/Actions.jsx
@@ -0,0 +1,18 @@
+function Actions() {
+  return (
+    <div className="actions">
+      <link
+        rel="stylesheet"
+        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+        crossOrigin="anonymous"
+      />
+      <i className="far fa-comment"></i>
+      <i className="fas fa-retweet"></i>
+      <i className="far fa-heart"></i>
+      <i className="fas fa-share"></i>
+    </div>
+  );
+}
+
+export default Actions;
